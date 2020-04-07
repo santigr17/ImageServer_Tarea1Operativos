e1dcfa327c07ee29e30e81d65ded3e97cd5a4be3
Esteban Ferarios <ferarios95@gmail.com>
2020-04-07 12:24:40 -0600

    Se cambia el servidor  al de tutorial
    Se cambia al servidor a del tutorial de GNU

--- Server/ImageServer.c
+++ Server/ImageServer.c
@@ -1,124 +1,81 @@
  
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+// Servidor basado en https://www.gnu.org/software/libmicrohttpd/tutorial.html#Hello-browser-example
+
 #include <sys/types.h>
+#include <sys/select.h>
 #include <sys/socket.h>
-#include <sys/sendfile.h>
-#include <netinet/in.h>
-#include <netdb.h>
-#include <arpa/inet.h>
-#include <string.h>
-#include <sys/stat.h>
-#include <fcntl.h>
+#include <microhttpd.h>
 
-//Función para obtener el tamaño de la imágen
-unsigned long fsize(char* file)
+#define PORT 8888
+
+//GNU libmicrohttpd llamará a la función que vamos a escribir ahora cada vez que ingrese una solicitud apropiada.
+
+int answer_to_connection (void *cls, struct MHD_Connection *connection,
+                          const char *url,
+                          const char *method, const char *version,
+                          const char *upload_data,
+                          size_t *upload_data_size, void **con_cls)
 {
-    FILE * f = fopen(file, "r");
-    fseek(f, 0, SEEK_END);
-    unsigned long len = (unsigned long)ftell(f);
-    fclose(f);
-    return len;
+  const char *page  = "<html><body>Hola Servidor!!</body></html>";
+  //Para obtener dicha respuesta, entregamos nuestros datos (la cadena de respuesta) y su tamaño a la función 
+  //MHD_create_response_from_buffer. Los dos últimos parámetros básicamente le dicen a MHD que no queremos que 
+  //elimine los datos del mensaje para nosotros cuando se ha enviado y que tampoco es necesario realizar una copia 
+  //interna porque la cadena constante no cambiará de todos modos.
+
+  struct MHD_Response *response;
+  int ret;
+
+  response = MHD_create_response_from_buffer (strlen (page),
+                                            (void*) page, MHD_RESPMEM_PERSISTENT);
+	/*
+	Ahora que la respuesta se ha unido, está lista para la entrega y se puede poner en cola para enviar. 
+	Esto se hace pasándolo a otra función libmicrohttpd de GNU. Como todo nuestro trabajo se realizó en 
+	el ámbito de una función, el destinatario es sin duda el asociado con la conexión de la variable local y, 
+	en consecuencia, esta variable se asigna a la función de cola. Cada respuesta HTTP va acompañada de 
+	un código de estado, aquí "OK", para que el cliente sepa que esta respuesta es el resultado previsto 
+	de su solicitud y no debido a algún error o mal funcionamiento.
+	*/										
+
+  ret = MHD_queue_response (connection, MHD_HTTP_OK, response);
+  MHD_destroy_response (response);
+
+  return ret;
 }
 
-//Contenido de la página principal
-char paginaweb[] = 
-"HTTP/1.1 200 Ok\r\n"
-"Content-Type: text/html; charset=UTF-8\r\n\r\n"
-"<!DOCTYPE html>\r\n"
-"<html><head><title>Servidor Web en C</title>\r\n"
-"<style>body { background-color: #A9D0F5 }</style></head>\r\n"
-"<body><center><h1>Hola mundo!</h1><br>\r\n"
-"<img src=\"icono.png\"/></center></body></html>\r\n";
+//Main
 
-//Función principal
-int main(int argc, char *argv[]) 
-	{
-	struct sockaddr_in server_addr, client_addr; 
-	socklen_t sin_len =sizeof(client_addr);
-	int fd_server, fd_client;
-	char buf[2048];
-	int fdimg;
-	int on = 1;
-        
-        /*Usamos socket como descriptor de archivo, con el dominio que será AF_INET para usar los protocolos
-        de ARPA de internet, luego el type, donde usaremos la variable SOCK_STREAM socket de flujo, y por último
-        el protocólo que será 0*/
-	fd_server = socket(AF_INET, SOCK_STREAM, 0); 
-        
-	if (fd_server < 0) {
-		perror("socket");
-		exit(1);	
-	}
-        
-        /* Establecemos las opciones del socket, le pasamos nuestro socket, luego SOL_SOCKET primera capa
-        de socket usado para opciones independientes al socket, SO_REUSEADDR especifica las reglas de validación
-        de las direcciones suministradas a bind() es un valor booleano, luego le pasamos el valor on = 1, y el
-        tamaño del sock */
-	setsockopt(fd_server, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(int));
-	
-        //Familia de la dirección
-	server_addr.sin_family = AF_INET;
-	server_addr.sin_addr.s_addr = INADDR_ANY;
-	server_addr.sin_port = htons(8080);
-	
-	if (bind(fd_server, (struct sockaddr *) &server_addr, sizeof(server_addr)) == -1)
-	{
-		perror("bind");
-		close(fd_server);
-		exit(1);
-	}
+int main ()
+{
+  struct MHD_Daemon *daemon;
 
-	if (listen(fd_server, 10) == -1)
-	{
-		perror("listen");
-		close(fd_server);
-		exit(1);
-	}	
-	
-        //Loop principal que se ejcuta en cada conexión
-	while(1) {
-                //Con accpet aceptamos las conexiónes entrantes
-		fd_client = accept(fd_server, (struct sockaddr *) &client_addr, &sin_len);
-		if (fd_client == 1)
-		{
-			perror("Conexión fallida...\n");
-			continue;
-		}	
-		printf("Conexión establecida.\n");
+  daemon = MHD_start_daemon (MHD_USE_INTERNAL_POLLING_THREAD, PORT, NULL, NULL,
+                             &answer_to_connection, NULL, MHD_OPTION_END);
+  if (NULL == daemon) return 1;
 
-                //Levantamos child process
-		if (!fork()) {
-			close(fd_server);
-			memset(buf, 0, 2048);
-			read(fd_client, buf, 2047);
-                        //si el get nos pide el favicon.ico se lo mandamos
-			if(!strncmp(buf, "GET /favicon.ico", 16))
-			{
-				fdimg = open("favicon.ico", O_RDONLY);
-				sendfile(fd_client, fdimg, NULL, fsize("favicon.ico"));
-				close(fdimg);
+/*
 
-                        //Cuando nos pide la imagen se la mandamos para que pueda mostrarse
-			} else if(!strncmp(buf, "GET /icono.png", 14)) {
-				fdimg = open("icono.png", O_RDONLY);
-				sendfile(fd_client, fdimg, NULL, fsize("icono.png"));
-				close(fdimg);
+El primer parámetro es uno de los tres modos de operación posibles. Aquí queremos que el demonio se ejecute en un hilo separado 
+y que administre todas las conexiones entrantes en el mismo hilo. Esto significa que mientras se produce la respuesta para una 
+conexión, las otras conexiones se pondrán en espera. En este ejemplo, donde la respuesta ya se conoce y, por lo tanto, la solicitud 
+se atiende rápidamente, esto no plantea ningún problema.Permitiremos que todos los clientes se conecten independientemente de su 
+nombre o ubicación, por lo tanto, no los verificamos en la conexión y establecemos el tercer y cuarto parámetro en NULL.El parámetro
+ cinco es la dirección de la función que queremos que se llame siempre que se establezca una nueva conexión. 
+ 
+Nuestra answer_to_connection sabe mejor lo que el cliente quiere y no necesita información adicional (que podría pasarse a través
+del siguiente parámetro), por lo que el siguiente (sexto) parámetro es NULL. Del mismo modo, no necesitamos pasar opciones 
+adicionales al demonio, así que solo escribimos el MHD_OPTION_END como último parámetro. A medida que el daemon del servidor se 
+ejecuta en segundo plano en su propio hilo, el flujo de ejecución en nuestra función principal se detendrá justo después de la 
+llamada. Debido a esto, debemos retrasar el flujo de ejecución en el hilo principal o de lo contrario el programa terminará 
+prematuramente. Dejamos que se detenga de manera amigable con el tiempo de procesamiento esperando que se presione la tecla Intro. 
+Al final, detenemos al demonio para que pueda realizar sus tareas de limpieza.
+*/
 
-                        //Para cuando el GET sea para /
-			} else  {
-				write(fd_client, paginaweb, sizeof(paginaweb) -1);
 
-			}
-			
-			close(fd_client);
-			printf("Cerrando...\n");
-			exit(0);
-			
-		}
-		close(fd_client);
-	}	
+getchar ();
 
-	return 0;
+  MHD_stop_daemon (daemon);
+  return 0;
 }
+
+
+
